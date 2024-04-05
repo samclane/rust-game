87d2a9c76d8924be495ce9a8f78699a8d755c57e
Sawyer McLane <samclane@gmail.com>
2024-04-05 09:09:38 -0600

    Refactor behaviors module to include NormalWalker behavior

--- src/behaviors.rs
+++ src/behaviors.rs
@@ -1,6 +1,6 @@
 use bevy::prelude::*;
 
-use rand_distr::{Distribution, Normal};
+use rand_distr::{Distribution, Normal, StandardNormal};
 
 use rand::distributions::Uniform;
 use rand::Rng;
@@ -11,11 +11,14 @@ use crate::{movement::Acceleration, schedule::InGameSet};
 pub enum WalkerType {
     RandomWalker,
     GaussianWalker,
+    NormalWalker,
 }
 
 impl WalkerType {
     pub fn get_random() -> Self {
-        match rand::thread_rng().gen_range(0..2) {
+        let count = 3;
+        match rand::thread_rng().gen_range(0..count) {
+            1 => Self::NormalWalker,
             0 => Self::GaussianWalker,
             _ => Self::RandomWalker,
         }
@@ -36,7 +39,7 @@ impl Plugin for BehaviorsPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(
             Update,
-            (random_walk, gaussian_walk).in_set(InGameSet::EntityUpdates),
+            (random_walk, gaussian_walk, normal_walk).in_set(InGameSet::EntityUpdates),
         );
     }
 }
@@ -50,13 +53,23 @@ fn random_walk(mut query: Query<&mut Acceleration, With<WalkerType>>) {
     }
 }
 
-fn gaussian_walk(mut query: Query<&mut Acceleration, With<WalkerType>>) {
+fn walk<T: Distribution<f32>>(
+    mut query: Query<&mut Acceleration, With<WalkerType>>,
+    distribution: T,
+) {
     let mut rng = rand::thread_rng();
-    let Ok(normal) = Normal::new(0.0, 1.0) else {
-        panic!("Failed to create normal distribution");
-    };
     for mut acceleration in query.iter_mut() {
-        let (x, z) = (normal.sample(&mut rng), normal.sample(&mut rng));
+        let (x, z) = (distribution.sample(&mut rng), distribution.sample(&mut rng));
         acceleration.value += Vec3::new(x, 0., z);
     }
+}
+
+fn gaussian_walk(query: Query<&mut Acceleration, With<WalkerType>>) {
+    let distribution = StandardNormal;
+    walk(query, distribution);
+}
+
+fn normal_walk(query: Query<&mut Acceleration, With<WalkerType>>) {
+    let distribution = Normal::new(0.0, 0.5).unwrap();
+    walk(query, distribution);
 }
