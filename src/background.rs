3cfb975610d5fc1f699562d518aefe2a434e8442
Sawyer McLane <samclane@gmail.com>
2024-04-08 10:53:06 -0600

    Refactor background rendering and noise generation

--- src/background.rs
+++ src/background.rs
@@ -4,11 +4,15 @@ use bevy::{
         Extent3d, TextureDescriptor, TextureDimension, TextureFormat, TextureUsages,
     },
 };
-use noise::{core::worley::ReturnType, Add, Fbm, MultiFractal, NoiseFn, Perlin, Worley};
+use noise::{
+    core::worley::ReturnType,
+    utils::{NoiseMapBuilder, PlaneMapBuilder},
+    Add, Cache, Fbm, MultiFractal, NoiseFn, Perlin, Worley,
+};
 use rand::Rng;
 
 const NEBULA_FREQUENCY: f64 = 2.0;
-const STAR_FREQUENCY: f64 = 100.0;
+const STAR_FREQUENCY: f64 = 1.0;
 const NEBULA_LACUNARITY: f64 = 3.0;
 const NEBULA_PERSISTENCE: f64 = 0.6;
 
@@ -52,20 +56,31 @@ fn render_background(
     // fill image.data with zeroes
     image.resize(size);
 
-    let mut rng = rand::thread_rng();
-    let seed = rng.gen_range(0..1000);
-    let nebula_base = Fbm::<Perlin>::new(seed)
-        .set_frequency(NEBULA_FREQUENCY)
-        .set_persistence(NEBULA_PERSISTENCE)
-        .set_lacunarity(NEBULA_LACUNARITY)
-        .set_octaves(5);
-    let stars = Worley::new(seed)
-        .set_frequency(STAR_FREQUENCY)
-        .set_return_type(ReturnType::Distance);
-    let nebula = Add::new(nebula_base, stars);
+    fn nebula_noise() -> impl NoiseFn<f64, 3> {
+        let mut rng = rand::thread_rng();
+        let seed = rng.gen_range(0..1000);
+        let nebula_base = Fbm::<Perlin>::new(seed)
+            .set_frequency(NEBULA_FREQUENCY)
+            .set_persistence(NEBULA_PERSISTENCE)
+            .set_lacunarity(NEBULA_LACUNARITY)
+            .set_octaves(5);
+
+        let stars = Worley::new(seed + 1)
+            .set_frequency(STAR_FREQUENCY)
+            .set_return_type(ReturnType::Distance);
+
+        let nebula = Add::new(nebula_base, stars);
+        Cache::new(nebula)
+    }
+    let nebula_noise = nebula_noise();
+    let noise_map = PlaneMapBuilder::new(&nebula_noise)
+        .set_size(size.width as usize, size.height as usize)
+        .set_x_bounds(-2.0, 2.0)
+        .set_y_bounds(-2.0, 2.0)
+        .build();
     for y in 0..size.height {
         for x in 0..size.width {
-            let value = nebula.get([x as f64 / 100.0, y as f64 / 100.0, 0.0]);
+            let value = noise_map.get_value(x as usize, y as usize);
             let value = (value * 0.5 + 0.5) * 255.0;
             let value = value as u8;
             let index = (y * size.width + x) as usize * 4;
@@ -81,8 +96,8 @@ fn render_background(
     let plane_handle = meshes.add(Plane3d::default().mesh().size(1000., 1000.));
     let plane_material_handle = materials.add(StandardMaterial {
         base_color_texture: Some(image_handle),
-        reflectance: 0.02,
-        unlit: false,
+        reflectance: 0.0,
+        unlit: true,
         ..default()
     });
 
